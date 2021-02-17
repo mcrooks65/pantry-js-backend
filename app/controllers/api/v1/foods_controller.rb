1d193abdd345b7dae66ffc492a84794d60439086
mcrooks65 <mfcrooks@gmail.com>
2021-02-17 15:04:58 -0800

    Add Food controller index and create routes

--- app/controllers/api/v1/foods_controller.rb
+++ app/controllers/api/v1/foods_controller.rb
@@ -1,6 +1,22 @@
 class Api::V1::FoodsController < ApplicationController
     def index
-        @foods = Food.all
-        render json: @foods
+        foods = Food.all
+        render json: foods
     end
+
+    def create
+        foods = Foods.new(foods_params)
+        if foods.save
+            render json: foods, status :accepted
+        else
+            render json: { errors: foods.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def foods_params
+        params.require(:foods).permit(:name, :category, :quantity, :pantry_id)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :foods, only: [:index]
+      resources :foods, only: [:index, :create]
     end
   end
 end
