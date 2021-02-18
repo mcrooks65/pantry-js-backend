11c641c1b1a847dd45de2c30d4b30cfa43eebe25
mcrooks65 <mfcrooks@gmail.com>
2021-02-18 15:41:18 -0800

    Add Pantry route index only

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :foods, only: [:index, :create]
+      resources :pantries, only: [:index]
     end
   end
 end
