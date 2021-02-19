838a26e221d0166d8899e3a0c6c6c43bf85d03f5
mcrooks65 <mfcrooks@gmail.com>
2021-02-18 16:09:27 -0800

    Add Pantry create to route & controller

--- app/controllers/api/v1/pantries_controller.rb
+++ app/controllers/api/v1/pantries_controller.rb
@@ -3,4 +3,20 @@ class Api::V1::PantriesController < ApplicationController
         pantries = Pantry.all
         render json: PantrySerializer.new(pantries)
     end
+
+    def create
+        pantry = Pantry.new(pantry_params)
+        if pantry.save
+            render json: PantrySerializer.new(pantry), status: :accepted
+        else
+            render json: { errors: pantry.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def pantry_params
+        params.require(:pantries).permit(:name)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :foods, only: [:index, :create]
-      resources :pantries, only: [:index]
+      resources :pantries, only: [:index, :create]
     end
   end
 end
