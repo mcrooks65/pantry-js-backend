28deb64e9b958d9bfc2359b29c47626092e411a9
mcrooks65 <mfcrooks@gmail.com>
2021-02-27 16:30:52 -0800

    Add delete route to foods in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :foods, only: [:index, :create]
+      resources :foods, only: [:index, :create, :destroy]
       resources :pantries, only: [:index, :create]
     end
   end
